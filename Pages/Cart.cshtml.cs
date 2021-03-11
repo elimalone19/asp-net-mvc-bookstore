59f38e51625feb0d4b70151921663a71663b06e7
elimalone19 <elimalone19@gmail.com>
2021-03-11 00:59:17 -0700

    Fixed cart summary

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -14,10 +14,10 @@ namespace asp_net_fifth_assignment.Pages
     {
         private IBookstoreRepository repository;
 
-        public CartModel (IBookstoreRepository repo, Cart cart)
+        public CartModel (IBookstoreRepository repo, Cart cartService)
         {
             repository = repo;
-            Cart = cart;
+            Cart = cartService;
         }
         
         public Cart Cart { get; set; }
@@ -26,18 +26,18 @@ namespace asp_net_fifth_assignment.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long bookId, string returnUrl)
         {
             Book book = repository.Books.FirstOrDefault(b => b.BookId == bookId);
 
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
 
             Cart.AddItem(book, 1);
 
-            HttpContext.Session.SetJson("cart", Cart);
+            //HttpContext.Session.SetJson("cart", Cart);
 
             return RedirectToPage(new { returnUrl = returnUrl });
         }
@@ -45,12 +45,12 @@ namespace asp_net_fifth_assignment.Pages
         public IActionResult OnPostRemove(long bookId, string returnUrl)
         {
 
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
 
             Cart.RemoveLine(Cart.Lines.First(b =>
                 b.Book.BookId == bookId).Book);
 
-            HttpContext.Session.SetJson("cart", Cart);
+            //HttpContext.Session.SetJson("cart", Cart);
 
             return RedirectToPage(new { returnUrl = returnUrl });
         }
